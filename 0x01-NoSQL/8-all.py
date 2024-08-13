13b745012b111a0231c429378e6b756e0869a96f
lucky baraka <luckybaraka21@gmail.com>
2024-08-13 14:02:14 +0000

    added the few things

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Write a Python function that lists all documents in a collection:
+
+Prototype: def list_all(mongo_collection):
+Return an empty list if no document in the collection
+mongo_collection will be the pymongo collection object
+
+"""
+
+
+def list_all(mongo_collection):
+	documents = mongo_collection.find()
+	return list(documents)
