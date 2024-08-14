e9847fcd0706d1e626b41ed78269243c2648ee2e
lucky baraka <luckybaraka21@gmail.com>
2024-08-14 10:24:40 +0000

    added the solution to the 11th challenge

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,17 +1,17 @@
-//Write a Python function that returns the list of school having a specific topic:
-
-//Prototype: def schools_by_topic(mongo_collection, topic):
-//mongo_collection will be the pymongo collection object
-//topic (string) will be topic searched
+#!/usr/bin/env python3
 
 
+"""
+Write a Python function that returns the list of school having a specific topic:
 
-
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be the pymongo collection object
+topic (string) will be topic searched
+"""
 
 
 def schools_by_topic(mongo_collection, topic):
 	"""
-    Prototype: def schools_by_topic(mongo_collection, topic):
-    Return list of schools having a specific topic
-    """
+	This function has to return list of schools having a specific topic
+	"""
 	return mongo_collection.find({ "topic": topic })
